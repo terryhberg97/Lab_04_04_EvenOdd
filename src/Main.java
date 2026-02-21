ed7770cc626c9f591818503caea03323f8587c69
terryhberg97 <terryhberg97@gmail.com>
2026-02-21 15:34:03 -0500

    Init

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+                int numToExamine =2;
+
+                if (numToExamine % 2 == 0) {
+                    System.out.println("The number is even");
+                } else {
+                    System.out.println("The number is odd");
+
+
+
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        }
+    }
+}
